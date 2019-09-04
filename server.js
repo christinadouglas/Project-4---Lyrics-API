d38a256354a90f71d46392b35d642de49cd8162d
Christina Douglas <chrissyidouglas@gmail.com>
2019-09-04 11:10:25 -0700

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express        = require('express');
 const app            = express();
+const cors           = require('cors');
 const bodyParser     = require('body-parser');
 
 require('./db/db');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const songController = require('./controllers/songController');
+
+app.use('/api/v1/songs', songController);
 
 app.listen(process.env.PORT || 9000, () => {
     console.log('listening on port 9000');
